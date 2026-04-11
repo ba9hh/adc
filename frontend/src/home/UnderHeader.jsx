e458e608e77f9040d106c9f636d8436cfa33b7ce
ba9hh <zedtourheart@gmail.com>
2026-04-11 21:24:09 +0100

    first commit

--- frontend/src/home/UnderHeader.jsx
+++ frontend/src/home/UnderHeader.jsx
@@ -14,7 +14,7 @@ const UnderHeader = () => {
     //   <img src={kiwi1} className="w-1/5 h-full mt-10" />
     //   <img src={karmous} className="w-1/5 h-full" />
     // </div>
-    <div className="flex md:px-0 mt-6">
+    <div className="flex md:px-0 mt-2">
       <img src={aneb} className="w-1/5 h-full" />
       <img src={dele3} className="w-1/5 h-full pt-10" />
       <img src={fruits} className="w-1/5 h-full pt-20" />
